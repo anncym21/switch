9e31be706316ff2c6bb833c2790b2d931d231f83
student <cymanowska.anna@wp.pl>
2021-11-19 12:07:48 +0100

    działający program

--- app/src/main/java/com/example/zadanie2/MainActivity.kt
+++ app/src/main/java/com/example/zadanie2/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.zadanie2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.zadanie2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setCheckedChangeListener()
+    }
+
+    private fun setCheckedChangeListener() {
+        binding.switchID.setOnCheckedChangeListener { _, isChecked ->
+            val msg = getString(if (isChecked) R.string.on else R.string.off)
+            Toast.makeText(this@MainActivity, msg, Toast.LENGTH_SHORT).show()
+            binding.switchID.text = msg
+        }
     }
 }
